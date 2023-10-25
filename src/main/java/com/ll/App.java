30889a523320da8fa057c4b9b61342218330d18d
JeUk Park <qkrwpdnr4531@naver.com>
2023-10-25 15:37:38 +0900

    ëApp 클래스 업데이트->입력 추가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,16 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
     void run(){
-        System.out.println("프로그램 실행");
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
+
+        Scanner scanner = new Scanner(System.in);
+        String sc = scanner.nextLine();
+
+        System.out.printf("입력하신긴 명령 : %s\n", sc);
     }
 }
