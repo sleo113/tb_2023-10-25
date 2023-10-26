c8e0d875e3c84589a9db7cfc1406d3860e1af17d
JeUk Park <qkrwpdnr4531@naver.com>
2023-10-26 10:28:33 +0900

    ëApp클래스 업데이트->등록, 목록

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,16 +1,52 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
+    List<String> wise_sayings= new ArrayList<>();
+    List<String> authors= new ArrayList<>();
+
     void run(){
-        System.out.println("== 명언 앱 ==");
+        int a = 0;
 
-        System.out.print("명령) ");
+        while(true){
+            System.out.println("== 명언 앱 ==");
 
-        Scanner scanner = new Scanner(System.in);
-        String sc = scanner.nextLine();
+            System.out.print("명령) ");
 
-        System.out.printf("입력하신긴 명령 : %s\n", sc);
+            Scanner scanner = new Scanner(System.in);
+            String sc = scanner.nextLine();
+
+            if(sc.equals( "등록")){
+                System.out.print("명언 : ");
+                String wise_saying = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+                System.out.println("\n");
+
+                wise_sayings.add(wise_saying);
+                authors.add(author);
+
+                a++;
+
+                System.out.println(a + "번 명언이 등록되었습니다.");
+                System.out.println("\n");
+            } else if (sc.equals( "목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for(int i = wise_sayings.size() - 1; i >=0; i--){
+                    System.out.println((i + 1) + " / " + authors.get(i) +" / "+ wise_sayings.get(i));
+                }
+
+            } else if( sc.equals( "종료")){
+                break;
+            }
+            System.out.printf("입력하신긴 명령 : %s\n", sc);
+        }
     }
 }
