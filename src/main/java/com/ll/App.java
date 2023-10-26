70b578bed4fba619ea6959eff741513bfcfa19fa
JeUk Park <qkrwpdnr4531@naver.com>
2023-10-26 11:15:39 +0900

    App 클래스 ì삭제 만드는 중

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,21 +6,22 @@ import java.util.Scanner;
 
 public class App {
 
-    List<String> wise_sayings= new ArrayList<>();
-    List<String> authors= new ArrayList<>();
+    List<String> wise_sayings = new ArrayList<>();
+    List<String> authors = new ArrayList<>();
 
-    void run(){
+    void run() {
+        int i;
         int a = 0;
 
-        while(true){
-            System.out.println("== 명언 앱 ==");
+        System.out.println("== 명언 앱 ==");
 
+        while (true) {
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
             String sc = scanner.nextLine();
 
-            if(sc.equals( "등록")){
+            if (sc.equals("등록")) {
                 System.out.print("명언 : ");
                 String wise_saying = scanner.nextLine();
 
@@ -35,18 +36,25 @@ public class App {
 
                 System.out.println(a + "번 명언이 등록되었습니다.");
                 System.out.println("\n");
-            } else if (sc.equals( "목록")) {
+
+            } else if (sc.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
 
-                for(int i = wise_sayings.size() - 1; i >=0; i--){
-                    System.out.println((i + 1) + " / " + authors.get(i) +" / "+ wise_sayings.get(i));
+                for (i = wise_sayings.size() - 1; i >= 0; i--) {
+                    System.out.println((i + 1) + " / " + authors.get(i) + " / " + wise_sayings.get(i));
                 }
 
-            } else if( sc.equals( "종료")){
+            }else if (sc.equals("삭제")){
+                for (i = wise_sayings.size() - 1; i >= 0; i--) {
+                    System.out.println((a + " / " + authors.get(i) + " / " + wise_sayings.get(i)));
+                }
+                System.out.print("\n");
+
+            }else if (sc.equals("종료")) {
                 break;
             }
-            System.out.printf("입력하신긴 명령 : %s\n", sc);
+            System.out.printf("입력하신 명령 : %s\n", sc);
         }
     }
 }
